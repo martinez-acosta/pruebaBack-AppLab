eb7a46d942d755d6bb515becdf98aaa38aec77ec
Lalo Mtz <fx2013630461@gmail.com>
2021-04-06 23:06:44 -0500

    Agregados archivos faltantes

---
+++ apis/__init__.py
@@ -0,0 +1,14 @@
+from flask_restplus import Api
+
+from .admin import api as ns_admin
+
+api = Api(
+  title='Prueba de back AppLab',
+  version='0.1.0',
+  description='Preuba de API de AppLab <style>.models {display: none !important}</style>',
+  contact="Eduardo Acosta Martinez",
+  contact_email="fx2013630461@gmail.com",
+  RESTPLUS_MASK_SWAGGER=False
+)
+
+api.add_namespace(ns_admin)
